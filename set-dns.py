558061a0419d9773e68ae5dbdfb48dc0260b6cde
jzheadley <headleyjz@vcu.edu>
2018-12-23 23:44:03 -0500

    added in a check so we don't needlessly add the same record

--- set-dns.py
+++ set-dns.py
@@ -2,8 +2,10 @@
 import CloudFlare 
 import os
 import requests
-
-def getCurrentIp():
+import socket
+def get_ip_by_dns(domain_name):
+    return socket.gethostbyname(domain_name)
+def get_current_ip():
     r = requests.get(r'http://jsonip.com')
     ip=r.json()['ip']
     return ip
@@ -14,8 +16,12 @@ def main():
     zone = os.environ['CF_ZONE_ID']
     target_record = os.environ['TARGET_RECORD']
     cf=CloudFlare.CloudFlare(email=email,token=token)
-    dns_record={'name':target_record,'type':'A','content':getCurrentIp()}
-    r = cf.zones.dns_records.post(zone, data=dns_record)
+    current_ip = get_current_ip()
+    if current_ip == get_ip_by_dns(target_record):
+        dns_record={'name':target_record,'type':'A','content':current_ip}
+        r = cf.zones.dns_records.post(zone, data=dns_record)
+    else:
+        print("not changing the record sicne its already correct")
     exit(0)
 
 if __name__ == '__main__':
